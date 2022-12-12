b6aa13f6a5ae00a1544ecd214cacb67afad1d94b
cali1204 <qwertyuiop.s1204@gmail.com>
2022-12-12 18:31:13 -0500

    setting up while loops

--- RockPaperScissors/ConsoleApp1/Program.cs
+++ RockPaperScissors/ConsoleApp1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerScore = 0;
+            int enemyScore = 0;
+            Console.WriteLine("Welcome to rock paper scissors!");
+            while (playerScore != 3 && enemyScore != 3)
+            {
+                Console.WriteLine("Player score - " + playerScore + ". Enemy score - " + enemyScore);
+                Console.WriteLine("Please enter 'r' for rock, 'p' for paper or anything else for scissors.");
+            }
+
+
+        }
+    }
+    }
+}
